0200c8d2212227c5ecbff2212e8a5bc0108dce41
sherwan24 <100037080+sherwan24@users.noreply.github.com>
2022-10-26 18:15:10 +0200

    Update Fitter.h

--- Fitter.h
+++ Fitter.h
@@ -5,6 +5,7 @@
 //  Created by Sherwan Jamo on 2022-10-26.
 //
 
+
 #include<iostream>
 #include <string>
 #include <vector>
@@ -19,22 +20,43 @@
 
 class fitter{
     public:
+    //Define the object/function attributes
+    int xmin;
+    int xmax;
+    std::string fit;  // “gaus” Gaussian function with 3 parameters: f(x) = p0*exp(-0.5*((x-p1)/p2)^2)
 
-    std::string fit;
+                      // “expo” An Exponential with 2 parameters: f(x) = exp(p0+p1*x)
+
+                      // “pol N” A polynomial of degree N: f(x) = p0 + p1*x + p2*x2 +...
+
     TObject::TH1D histogram; 
     TObject::TGraph graph;
     double x[];
     double y[];
     int n;
 
-    histogram_fitter(histogram, fit){
-        histogram-> TH1::Fit(fit,"R");
-        hist->Draw();
+    histogram_fitter(histogram, fit,xmin,xmax){
+        
+        if(fit = "gaus"){
+            histogram-> TH1::Fit(fit,"R","",xmin, xmax);
+            double constant = histogram->GetParameter(0);
+            double mean = histogram->GetParameter(1);
+            double sigma = histogram->GetParameter(2);
+            cout << "The constant is equal to "<< constant << endl;
+            cout << "The mean is equal to "<< mean << endl;
+            cout << "The standard deviation is equal to "<< sigma << endl;
+        }
+        
+        else{
+            histogram-> TH1::Fit(fit,"R","", xmin, xmax);
+            
+        
+        
+        }
     }
 
-    graph_fitter(graph,fit){
-        graph->TH1::Fit(fit,"R");
-        g->Draw();
+    graph_fitter(graph,fit,xmin,xmax){
+        graph->TH1::Fit(fit,"R","",xmin, xmax);
     }
 };
 #endif /* Fitter_h */
